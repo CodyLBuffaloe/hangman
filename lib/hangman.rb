b615f17c711acdeec416610323763aab89d50d0f
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-11-02 14:36:35 -0700

    Populates array of underscores with correct guess letters

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -14,15 +14,33 @@ class Hangman
     return secret_word.downcase
   end
 
+  def find_letter_matches(secret_word, guess_letter_blanks, human_guess)
+    puts secret_word
+    puts guess_letter_blanks.join()
+    puts human_guess
+    secret_word.split(//).each_with_index do |letter, index|
+      if(letter == human_guess)
+        guess_letter_blanks[index] = letter
+      end
+    end
+
+  end
+
   def run()
     secret_word = pick_random_word()
+    guess_letter_blanks = []
+    human_guess = nil
+    (secret_word.length).times do
+      guess_letter_blanks << "_ "
+    end
     while @guesses > 0
       puts "guesses: #{@guesses}"
-      (secret_word.length).times do
-        print "_ "
-      end
+      puts guess_letter_blanks.join()
       print "\n"
+      print "Guess a letter:"
+      human_guess = gets.chomp
       @guesses -= 1
+      find_letter_matches(secret_word, guess_letter_blanks, human_guess)
     end
 
   end
